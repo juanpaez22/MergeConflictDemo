f270a1af24f2a6044a20a8bd76ef0d8cbc418e23
Juan Paez <juanpaez@utexas.edu>
2020-09-17 23:41:38 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
+import java.lang.Math;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isPrime(50));
-        System.out.println(isPrime(123));
-        System.out.println(isPrime(37));
+        System.out.println(isPrime(7));
+        System.out.println(isPrime(124323));
+        System.out.println(isPrime(3247));
+        System.out.println(isPrime(54));
+        System.out.println(isPrime(42));
+        System.out.println(isPrime(11));
     }
 
     public static void isPrime(int x) {
-        for (int i = 2; i < x; i ++) {
-            if (x % i == 0) {
-                return false;
-            }
+        if (i < 2) return false;
+        for (int i = 2; i < Math.sqrt(x; i ++) {
+            if (x % i == 0) return false;
         }
-        return true;
     }
 }
 
