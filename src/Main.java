dc50f7b1e35ba79cdb314a4c54e57fcbe06591ef
Juan Paez <juanpaez@utexas.edu>
2020-09-17 23:31:57 -0500

    isprimme

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,18 @@ package r2.src;
 
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println(isPrime(50));
+        System.out.println(isPrime(123));
+        System.out.println(isPrime(37));
+    }
+
+    public static void isPrime(int x) {
+        for (int i = 2; i < x; i ++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
