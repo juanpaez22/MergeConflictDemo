69ead6b9fdf0fe12ec54b3d0625d0f65001d0633
Juan Paez <juanpaez@utexas.edu>
2020-09-18 09:17:51 -0500

    clean

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,5 @@ public class Main {
         System.out.println(isPrime(42));
         System.out.println(isPrime(11));
     }
-
-    public static void isPrime(int x) {
-        if (i < 2) return false;
-        for (int i = 2; i < Math.sqrt(x; i ++) {
-            if (x % i == 0) return false;
-        }
-    }
 }
 
